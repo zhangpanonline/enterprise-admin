df7af6efd1f0687590a7e8e22eb278ce48470ee9
zhangpan <zhangpan.online@outlook.com>
2025-09-10 11:09:15 +0800

    feat(主应用): 左侧菜单子应用选择添加动画

--- pc/main-app/src/App.vue
+++ pc/main-app/src/App.vue
@@ -5,8 +5,7 @@
       <!-- 应用切换 -->
       <div class="relative w-full pr-2.5 inline-block text-left">
         <div
-          class="flex py-3 pl-5 pr-2.5 cursor-pointer rounded-r-2xl items-center gap-5 justify-between w-full font-medium text-gray-700 
-          hover:bg-size-[100%_100%] from-gray-50 to-gray-100 bg-linear-to-r bg-size-[0%_100%] hover:transition-[background_size] ease-linear"
+          class="showMicroBtn relative flex py-3 pl-5 pr-2.5 cursor-pointer items-center gap-5 justify-between w-full font-medium text-gray-700 z-0 before:absolute before:inset-0 before:from-gray-900/6 before:to-gray-900/9 before:bg-linear-to-r before:scale-x-0 before:origin-left before:transition-transform before:duration-400 hover:before:scale-x-100 before:z-[-1] before:rounded-r-full"
           @click="showMicroApp = !showMicroApp"
         >
           <img :src="microList[activeMicro].icon" class="w-5 h-5" />
@@ -16,7 +15,7 @@
             fill="none"
             viewBox="0 0 24 24"
             stroke-width="1.5"
-            stroke="currentColor"
+            stroke="#9f9fa9"
             class="size-6"
           >
             <path
@@ -26,44 +25,35 @@
             />
           </svg>
         </div>
-        <div
-          v-if="showMicroApp"
-          class="absolute right-0 w-56 mt-2 origin-top-right bg-white rounded-md shadow-lg dark:bg-gray-800 ring-1 ring-black ring-opacity-5"
+        <Transition
+          enter-active-class="transition-all duration-400 ease-out"
+          enter-from-class="opacity-0 -translate-y-5"
+          enter-to-class="opacity-100 translate-y-0"
+          leave-active-class="transition-all duration-200 ease-in"
+          leave-from-class="opacity-100 translate-y-0"
+          leave-to-class="opacity-0 -translate-y-5"
         >
-          <div class="py-1">
-            <a
-              href="#"
-              class="block block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600"
-              role="menuitem"
-            >
-              <span class="flex flex-col">
-                <span> Stripe </span>
-              </span>
-            </a>
-            <a
-              href="#"
-              class="block block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600"
-              role="menuitem"
-            >
-              <span class="flex flex-col">
-                <span> Mastercard </span>
-              </span>
-            </a>
-            <a
-              href="#"
-              class="block block px-4 py-2 text-md text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:text-white dark:hover:bg-gray-600"
-              role="menuitem"
-            >
-              <span class="flex flex-col">
-                <span> Paypal </span>
-              </span>
-            </a>
+          <div
+            v-if="showMicroApp"
+            ref="microListRef"
+            class="absolute left-1/2 right-1 mt-2 bg-white rounded-md shadow-lg border border-gray-200"
+          >
+            <RouterLink v-for="({ path, icon }, key) in microList" :key="key" :to="path">
+              <p
+                class="relative px-4 py-2 text-md text-gray-700 flex items-center z-0 mb-0.5 before:absolute before:inset-0 before:bg-gray-100 before:scale-x-0 before:origin-left before:transition-transform before:duration-400 hover:before:scale-x-100 before:z-[-1] before:rounded-r-full"
+              >
+                <span class="flex-1 text-center">{{ key }}</span>
+                <img :src="icon" class="w-4 h-4" />
+              </p>
+            </RouterLink>
           </div>
-        </div>
+        </Transition>
       </div>
 
       <!-- 菜单 -->
-      <ul></ul>
+      <ul>
+
+      </ul>
     </div>
     <!-- 子应用 -->
     <RouterView class="flex-1"></RouterView>
@@ -71,6 +61,7 @@
 </template>
 
 <script setup lang="ts">
+import { onClickOutside } from '@vueuse/core'
 const showMicroApp = ref(false)
 const microList = {
   vue: {
@@ -86,7 +77,15 @@ const microList = {
     icon: 'https://github.com/nuxt.png',
   },
 }
+const microListRef = useTemplateRef<HTMLElement>('microListRef')
+onClickOutside(microListRef, () => (showMicroApp.value = false), {
+  ignore: ['.showMicroBtn'],
+})
 const activeMicro = 'vue'
+
+const menuList = [
+  {}
+]
 </script>
 
 <style lang="css" scoped>
