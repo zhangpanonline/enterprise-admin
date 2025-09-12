48aab8874eb8fa68a42ae013e59fdc4f8760fdd7
zhangpan <zhangpan.online@outlook.com>
2025-09-12 17:43:13 +0800

    fix(vue子应用): 打包出错

--- pc/sub-app-vue/global.d.ts
+++ pc/sub-app-vue/global.d.ts
@@ -1,9 +1,11 @@
 declare global {
-    interface $wujie {
-        bus: {
-            $on: (string, Function) => void,
-            $emit: (string, Function) => void,
-            $off: (string, Function) => void,
+    interface Window {
+        $wujie: {
+            bus: {
+                $on: (string, Function) => void,
+                $emit: (string, Function) => void,
+                $off: (string, Function) => void,
+            }
         }
     }
 }
--- pc/sub-app-vue/vite.config.ts
+++ pc/sub-app-vue/vite.config.ts
@@ -8,45 +8,39 @@ import tailwindcss from '@tailwindcss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-// import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    tailwindcss(),
-    // 自动引入 Vue API、Pinia 等
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-      imports: ['vue', 'vue-router'],
-      dts: 'src/auto-imports.d.ts', // 自动生成类型文件
-      dirs: [
-        "./src/api/index.ts"
-      ]
-    }),
+    plugins: [
+      vue(),
+      vueJsx(),
+      tailwindcss(),
+      // 自动引入 Vue API、Pinia 等
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+        imports: ['vue', 'vue-router'],
+        dts: 'src/auto-imports.d.ts', // 自动生成类型文件
+        dirs: ['./src/api/index.ts'],
+      }),
 
-    // 自动注册组件（ElementPlus 等）
-    Components({
-      resolvers: [ElementPlusResolver()],
-      dts: 'src/components.d.ts',
-    }),
-    // vueDevTools({
-    //   componentInspector: false
-    // }),
-  ],
-  server: {
-    port: 1003,
-    host: true,
-    https: {
-      key: readFileSync(fileURLToPath(new URL('../key.pem', import.meta.url))),
-      cert: readFileSync(fileURLToPath(new URL('../cert.pem', import.meta.url))),
-      passphrase: 'zhangpan',
+      // 自动注册组件（ElementPlus 等）
+      Components({
+        resolvers: [ElementPlusResolver()],
+        dts: 'src/components.d.ts',
+      }),
+    ],
+    server: {
+      port: 1003,
+      host: true,
+      https: {
+        key: readFileSync(fileURLToPath(new URL('../key.pem', import.meta.url))),
+        cert: readFileSync(fileURLToPath(new URL('../cert.pem', import.meta.url))),
+        passphrase: 'zhangpan',
+      },
     },
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-})
+  })
