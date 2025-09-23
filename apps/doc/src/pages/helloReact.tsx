9bb848122f36ab6a27da6b51cccd680fbfd31ef1
zhangpan <zhangpan.online@outlook.com>
2025-09-23 18:30:04 +0800

    docs(文档变更): 测试

---
+++ apps/doc/src/pages/helloReact.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function Hello() {
+  return (
+    <Layout title="Hello" description="Hello React Page">
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: '20px',
+          height: '50vh',
+          border: '1px solid red'
+        }}>
+        <p>
+          Edit <code>pages/helloReact.js</code> and save to reload.
+        </p>
+      </div>
+    </Layout>
+  );
+}
