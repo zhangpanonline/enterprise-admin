db24f4dc71d9dcf8604d99436a0d406c43bb8806
zhangpan <zhangpan.online@outlook.com>
2025-10-13 22:35:23 +0800

    feat(服务端): 装饰圈、模块学习

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './order/order.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, OrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
